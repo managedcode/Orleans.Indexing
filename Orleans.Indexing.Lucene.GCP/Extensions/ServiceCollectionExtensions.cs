2d41df3f4d6ce334daddbb2e2d3a97799aa58980
dtymakhov <dtymakhov@gmail.com>
2022-04-26 15:24:30 +0300

    Added new projects

---
+++ Orleans.Indexing.Lucene.GCP/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,9 @@
+namespace Orleans.Indexing.Lucene.Extensions;
+
+public class ServiceCollectionExtensions
+{
+    // public static IServiceCollection AddFileSystemStorage(this IServiceCollection serviceCollection, LuceneOptions options)
+    // {
+    //     return serviceCollection.AddScoped<IFileSystemStorage>(_ => new FileSystemStorage(options));
+    // }
+}
